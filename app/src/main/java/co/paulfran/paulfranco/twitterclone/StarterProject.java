224c36d5dffe9806ab2921b8a5f2cd1f6689dcfa
Paul Franco <paulfranco@me.com>
2018-04-03 03:17:13 -0700

    Initial commit

---
+++ app/src/main/java/co/paulfran/paulfranco/twitterclone/StarterProject.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package co.paulfran.paulfranco.twitterclone;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterProject extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("91c13d4220dd7dc12efc6d441b27559c136d62dc")
+                .clientKey("9406525007750e435bb83f370ea381f63c22e5ef")
+                .server("http://18.188.147.221:80/parse")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback () {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
+
